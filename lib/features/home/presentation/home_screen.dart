174f9f6af31e3e1b81d61eeb365384c393b56b34
shuvo <hawladarshuvo@gmail.com>
2026-08-11 16:35:20 +0600

    start

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -10,7 +10,6 @@ class HomeScreen extends StatelessWidget {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
 
-
     return Scaffold(
       backgroundColor: const Color(0xFFF8FAFC),
       body: SafeArea(
